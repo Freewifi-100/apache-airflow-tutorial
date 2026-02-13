52e72b521625c48a1ef8d88c197b1e68a57384b6
Peeraya Khantaruangsakul <peeraya.4869@gmail.com>
2026-02-13 21:36:29 +0700

    parallel and condition completed

---
+++ dags/06_parallel_tasks.py
@@ -0,0 +1,59 @@
+from airflow.sdk import dag, task
+
+@dag(
+     dag_id="parallel_dag",
+)
+def parallel_dag():
+    @task.python
+    def extract_task(**kwargs):
+        print("Extracting data...")
+        ti = kwargs['ti']
+        extracted_data_dict = {"api_extracted_data": [1, 2, 3],
+                "db_extracted_data": [4, 5, 6],
+                "s3_extracted_data": [7, 8, 9]}
+        ti.xcom_push(key='return_value', value=extracted_data_dict)
+    
+    @task.python
+    def transform_task_api(**kwargs):
+        ti = kwargs['ti']
+        api_extracted_data = ti.xcom_pull(task_ids='extract_task', key='return_value')['api_extracted_data']
+        print(f"Transforming API data: {api_extracted_data}.....")
+        transformed_api_data = [x * 10 for x in api_extracted_data]
+        ti.xcom_push(key='return_value', value=transformed_api_data)
+    
+    @task.python
+    def transform_task_db(**kwargs):
+        ti = kwargs['ti']
+        db_extracted_data = ti.xcom_pull(task_ids='extract_task', key='return_value')['db_extracted_data']
+        print(f"Transforming DB data: {db_extracted_data}.....")
+        transformed_db_data = [x * 100 for x in db_extracted_data]
+        ti.xcom_push(key='return_value', value=transformed_db_data)
+    
+    @task.python
+    def transform_task_s3(**kwargs):
+        ti = kwargs['ti']
+        s3_extracted_data = ti.xcom_pull(task_ids='extract_task', key='return_value')['s3_extracted_data']
+        print(f"Transforming S3 data: {s3_extracted_data}.....")
+        transformed_s3_data = [x * 1000 for x in s3_extracted_data]
+        ti.xcom_push(key='return_value', value=transformed_s3_data)
+
+    @task.bash
+    def load_task(**kwargs):
+        print("Loading transformed data...")
+        api_data = kwargs['ti'].xcom_pull(task_ids='transform_task_api', key='return_value')
+        db_data = kwargs['ti'].xcom_pull(task_ids='transform_task_db', key='return_value')
+        s3_data = kwargs['ti'].xcom_pull(task_ids='transform_task_s3', key='return_value')
+
+        return f"echo 'Loaded Data: {api_data}, {db_data}, {s3_data}'"
+
+    #  defining task dependencies
+    extract = extract_task()
+    transform_api = transform_task_api()
+    transform_db = transform_task_db()
+    transform_s3 = transform_task_s3()
+    load = load_task()
+    
+    extract >> [transform_api, transform_db, transform_s3] >> load
+
+# Instantiate the DAG
+parallel_dag()
